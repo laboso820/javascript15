ac21cbcfa8a4430551e0c8605eee2e9f4b4cbdd7
laboso820 <obadiahlaboso820@gmail.com>
2025-07-31 22:03:24 +0300

    added donation file

---
+++ donation.js
@@ -0,0 +1,39 @@
+console.log("donation file")
+const donation=[
+    {
+    name: "James",
+    amount: 210,
+    currency: "Kes",
+  },
+  {
+    name: "Miriam",
+    amount: 200,
+    currency: "usd",
+  },
+  {
+    name: "Kevin",
+    amount: 700,
+    currency: "Kes",
+  },
+  {
+    name: "Leonard",
+    amount: 450,
+    currency: "usd",
+  },
+  {
+    name: "Riz",
+    amount: 400,
+    currency: "usd",
+  },
+]
+// const user={amount,currency}
+function calculateDonatios(){
+    let total=0
+let {amount , currency , name} = donation[]
+    for(i=0;i=donation.length; i++){
+    result=total+donation[i]
+    return result
+    }
+}
+// console.log(donation.length)
+calculateDonatios()
